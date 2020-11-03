25aef787bc20ada7603507a16e2017478f3b1a8f
mlm52 <mlm52@njit.edu>
2020-11-03 22:03:49 +0000

    add basic py file to put unmocked tests into

---
+++ tests/unmocked-unit-tests.py
@@ -0,0 +1,11 @@
+import unittest
+import os, sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import app
+
+class TestCase(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
