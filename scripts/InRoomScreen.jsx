91ec08394fea2af8a3b60afb9bb8f72bbaa275f9
mlm52 <mlm52@njit.edu>
2020-11-11 02:05:26 +0000

    linted jsx files I touched

--- scripts/InRoomScreen.jsx
+++ scripts/InRoomScreen.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Socket from './Socket';
 import Flashcards from './Flashcards';
 import Chatbox from './Chatbox';
-import WhiteboardButton from './WhiteboardButton'
+import WhiteboardButton from './WhiteboardButton';
 
 function InRoomScreen() {
   const [users, setUsers] = React.useState([]);
@@ -11,34 +11,33 @@ function InRoomScreen() {
       msg: '',
     });
   }
+
+  function updateUsers(data) {
+    console.log(`Received new user: ${data.all_users}`);
+    setUsers(data.all_users);
+  }
+
   function getNewuser() {
     React.useEffect(() => {
       Socket.on('users received', updateUsers);
       return () => {
-          Socket.off('users received', updateUsers);
-      }
+        Socket.off('users received', updateUsers);
+      };
     });
-      
   }
-  
-  function updateUsers(data) {
-    console.log('Received new user: ' + data['all_users']);
-    setUsers(data['all_users']);
-  }
-  
+
   getNewuser();
-      
+
   return (
     <div id="inRoomScreen">
       <p>Insert room HTML and components here</p>
-      <WhiteboardButton/>
+      <WhiteboardButton />
       <p>Chatbox</p>
       <p>
         <Chatbox />
         <ul className="userLis">
-        {
-            users.map((user, index) =>
-            <li key={index}>{user}</li>)
+          {
+            users.map((user, index) => <li key={index}>{user}</li>)
         }
         </ul>
         <Flashcards />
