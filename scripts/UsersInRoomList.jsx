7ae1ba85ced568fc0012055e99cd4c34a4e773cf
Jason Molisani <jm979@njit.edu>
2020-11-21 21:18:06 +0000

    kick user functionality added. There is still a minor issue with doubled 'kicked' emit, but this functions

--- scripts/UsersInRoomList.jsx
+++ scripts/UsersInRoomList.jsx
@@ -4,6 +4,7 @@ import Socket from './Socket';
 function UsersInRoomList() {
   const [users, setUsers] = React.useState([]);
   const [picUrls, setUrls] = React.useState([]);
+  const [userIds, setIds] = React.useState([]);
 
   function updateUsers(data) {
     console.log(`Received new user: ${data.all_users}`);
@@ -16,8 +17,16 @@ function UsersInRoomList() {
       Socket.on('users received', (data) => {
         setUsers(data.all_users);
         setUrls(data.all_user_pics);
-      })
+        setIds(data.all_user_ids);
+      });
     });
+  }
+  
+  function kickUser(kickTargetId) {
+    Socket.emit("kick user request", {
+      kickedUserId: kickTargetId,
+    });
+    
   }
 
   getNewUser();
@@ -27,7 +36,12 @@ function UsersInRoomList() {
       <ul className="userListing">
         <h1 className="UserTitle">Users:</h1>
         {
-          users.map((user, index) => <li key={index}><img src={picUrls[index]} className="img"></img> {user}</li>)
+          users.map((user, index) =>
+          <li key={index}>
+            <img src={picUrls[index]} className="img"></img>
+            <p>{user}</p>
+            <button onClick={(event) => kickUser(userIds[index][0])} type="submit">Kick</button>
+          </li>)
         }
       </ul>
     </div>
