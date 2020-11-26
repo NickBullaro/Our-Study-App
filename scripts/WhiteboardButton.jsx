02c55b69f1cac51ca16d3f048fdcd4d551421aee
mlm52 <mlm52@njit.edu>
2020-11-26 03:07:25 +0000

    made preperations for saving whiteboard

--- scripts/WhiteboardButton.jsx
+++ scripts/WhiteboardButton.jsx
@@ -1,21 +1,57 @@
 import * as React from 'react';
 import Whiteboard from './Whiteboard';
+import Socket from './Socket';
 
 function WhiteboardButton() {
   const [display, setDisplay] = React.useState(false);
-
+  const [picked, setPicked] = React.useState(false);
+  const [whiteboards, setWhiteboards] = React.useState([]);
+  const [boardPicked, setBoardPicked] = React.useState(0)
+  
   function switchDisplay() {
+    if(display)
+    {
+      Socket.emit("get whiteboards");
+    }
+    else
+    {
+      setPicked(false)
+    }
     setDisplay(!display);
   }
+  
+  function fGotWhiteboards(data) {
+    setWhiteboards(data);
+  }
+  function newGotWhiteboards() {
+    Socket.on('got whiteboard', fGotWhiteboards);
+    return () => Socket.off('got whiteboard', fGotWhiteboards);
+  }
+  React.useEffect(newGotWhiteboards);
+
+  
 
   return (
     <div id="whiteboard_component">
       <button type="button" onClick={switchDisplay}>Whiteboard</button>
-      {
-       display
-         ? <Whiteboard />
-         : <div />
-     }
+      {{
+        if(display)
+        {
+          if(picked)
+          {
+            <Whiteboard board={boardPicked}/>
+          }
+          else
+          {
+            <form>
+              <label>New Board:
+                <input id="new" name="new" type="text" />
+              </label>
+              <input type="submit" id="new" name="new" value="Create" />
+            </form>
+          }
+        }
+     }}
     </div>
   );
 }
