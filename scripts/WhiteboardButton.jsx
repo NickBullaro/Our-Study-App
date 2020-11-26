8ee596f2b6a49853ebde1ba4e9eb2afbbfce9cef
mlm52 <mlm52@njit.edu>
2020-11-26 03:20:31 +0000

    fixed error not rendering content

--- scripts/WhiteboardButton.jsx
+++ scripts/WhiteboardButton.jsx
@@ -6,8 +6,7 @@ function WhiteboardButton() {
   const [display, setDisplay] = React.useState(false);
   const [picked, setPicked] = React.useState(false);
   const [whiteboards, setWhiteboards] = React.useState([]);
-  const [boardPicked, setBoardPicked] = React.useState(0)
-  
+  const [boardPicked, setBoardPicked] = React.useState(false);
   function switchDisplay() {
     if(display)
     {
@@ -28,30 +27,32 @@ function WhiteboardButton() {
     return () => Socket.off('got whiteboard', fGotWhiteboards);
   }
   React.useEffect(newGotWhiteboards);
+  
+  function Picking(){
+      if(picked)
+      {
+        return (<Whiteboard board={boardPicked}/>)
+      }
+      return(
+        <form>
+          <label>New Board:
+            <input id="new" name="new" type="text" />
+          </label>
+          <input type="submit" id="new" name="new" value="Create" />
+        </form>
+      )
+  }
 
   
 
   return (
     <div id="whiteboard_component">
       <button type="button" onClick={switchDisplay}>Whiteboard</button>
-      {{
-        if(display)
-        {
-          if(picked)
-          {
-            <Whiteboard board={boardPicked}/>
-          }
-          else
-          {
-            <form>
-              <label>New Board:
-                <input id="new" name="new" type="text" />
-              </label>
-              <input type="submit" id="new" name="new" value="Create" />
-            </form>
-          }
-        }
-     }}
+      {
+       display
+         ? <Picking />
+         : <div />
+     }
     </div>
   );
 }
