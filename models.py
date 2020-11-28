190ec32ae9a9731aa2a018799d2b529fa64134c4
mlm52 <mlm52@njit.edu>
2020-11-28 21:24:27 +0000

    added new tables for whiteboards

--- models.py
+++ models.py
@@ -113,3 +113,23 @@ class EnteredRooms(DB.Model):
     def __init__(self, user, room):
         self.user = user
         self.room = room
+        
+class Whiteboards
+    id = DB.Column(DB.Integer, primary_key=True)
+    room = DB.Column(DB.Integer, DB.ForeignKey(Rooms.id), nullable=False)
+    name = DB.Column(DB.String(32), nullable=False)
+    
+    def __init__(self, room, name):
+        self.room = room
+        self.name = name
+
+class WhiteboardConnections
+    id = DB.Column(DB.Integer, primary_key=True)
+    whiteboard = DB.Column(DB.Integer, DB.ForeignKey(Whiteboards.id), nullable=False)
+    sid = DB.Column(DB.String(32), nullable=False)
+    leader = DB.Column(DB.Integer, nullable=False)
+    
+    def __init__(self, whiteboard, sid, leader):
+        self.whiteboard = room
+        self.sid = sid
+        self.leader = leader
